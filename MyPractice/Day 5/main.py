9866590ed8171b2d956bbef86112433fff1e5f40
Gleb-Salov <chmochma91@gmail.com>
2025-05-04 15:06:57 +0300

    Fix .gitignore to fully exclude __pycache__

--- MyPractice/Day 5/main.py
+++ MyPractice/Day 5/main.py
@@ -1,3 +1,5 @@
 from tools.formatter import capitalize_words
+from tools.stats import word_count
 
-print(capitalize_words("привет папка мейн"))
+print(capitalize_words("fast api старт"))
+print(word_count("fast api старт"))
